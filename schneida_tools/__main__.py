4f0c6ee5ca6066d3d2911fe3fa34799e3737969b
Adam Schneider <amschnei@uci.edu>
2022-03-15 16:11:18 -0700

    trivial change to run era5 analysis

--- schneida_tools/__main__.py
+++ schneida_tools/__main__.py
@@ -1,7 +1,9 @@
 from schneida_tools import clean_data
 from schneida_tools import analysis
 from schneida_tools import analysis_gswp3
+from schneida_tools import analysis_era5
 
 #clean_data.run()
-analysis_gswp3.run() # GSWP3 vs CRUNCEP7
-analysis.run() # CRUNCEP7 vs WFDE5
+#analysis_gswp3.run() # GSWP3 vs CRUNCEP7
+#analysis.run() # CRUNCEP7 vs WFDE5
+analysis_era5.run()
